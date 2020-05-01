2bf22db35bc62160a91fc55324d42099c415760e
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2020-05-01 15:18:08 -0300

    Reset

--- src/app.js
+++ src/app.js
@@ -1,25 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const session = require('cookie-session')
+
 const routes = require('./routes')
-const path = require('path')
 
 const app = express()
 
 app.use(cors())
-app.use(
-  session({
-    name: 'session',
-    keys: [
-      'bd7126f457237e4aab0d47124ce4aac2',
-      '9009def68579d15d871a5bf346422839',
-    ],
-    cookie: {
-      httpOnly: false,
-      expires: new Date(Date.now() + 60 * 60 * 1000 * 2), // 2 horas
-    },
-  }),
-)
 app.use(express.json())
 app.use(routes)
 
--- src/routes.js
+++ src/routes.js
@@ -1,40 +1,9 @@
-const validateToken = require('./middlewares/tokens')
 const express = require('express')
-const { MeliObject } = require('./utils')
-require('dotenv').config()
-
-const { PASSWORD } = process.env
 
 const routes = express.Router()
 
-routes.post('/login', (req, res) => {
-  if (req.body.password === PASSWORD) {
-    req.session.user = true
-    res.json({ ok: true })
-  } else {
-    res.json({ ok: false })
-  }
-})
+routes.post('/login', (req, res) => {})
 
-routes.get('/profile', validateToken, async (req, res) => {
-  try {
-    const meliObject = new MeliObject(res.locals.access_token)
-    const user = await meliObject.get('/users/me')
-    const categories = await meliObject.get(`/sites/${user.site_id}/categories`)
-    const currencies = await meliObject.get('/currencies')
-    const listing_types = await meliObject.get(
-      `/sites/${user.site_id}/listing_types`,
-    )
-    res.send().json({
-      user,
-      categories,
-      currencies,
-      listing_types,
-    })
-  } catch (err) {
-    console.log('Something went wrong', err)
-    res.status(500).send(`Error! ${err}`)
-  }
-})
+routes.get('/questions', async (req, res) => {})
 
 module.exports = routes
